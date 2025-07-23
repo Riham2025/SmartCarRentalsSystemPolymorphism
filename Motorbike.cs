7ed8d133d13a73274e23ed590d6fe0bed54c3284
riham <riham91.alsiyabi@gmail.com>
2025-07-23 15:20:22 +0400

    Indicates if the motorbike requires a helmet

---
+++ Motorbike.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCarRentalsSystemPolymorphism
+{
+    class Motorbike : Vehicle
+    {
+        public bool RequiresHelmet { get; set; } // Indicates if the motorbike requires a helmet
+    }
+}
