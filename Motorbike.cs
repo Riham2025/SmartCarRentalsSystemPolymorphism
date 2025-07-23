408378b01c6d4f2e96ca7a30f1356d9e61022d29
riham <riham91.alsiyabi@gmail.com>
2025-07-23 15:30:09 +0400

    public Motorbike(string brand, string model, int year, string licensePlate, bool requiresHelmet) // Constructor

--- Motorbike.cs
+++ Motorbike.cs
@@ -9,5 +9,11 @@ namespace SmartCarRentalsSystemPolymorphism
     class Motorbike : Vehicle
     {
         public bool RequiresHelmet { get; set; } // Indicates if the motorbike requires a helmet
+
+        public Motorbike(string brand, string model, int year, string licensePlate, bool requiresHelmet) // Constructor
+        : base(brand, model, year, licensePlate) 
+        {
+            RequiresHelmet = requiresHelmet;
+        }
     }
 }
