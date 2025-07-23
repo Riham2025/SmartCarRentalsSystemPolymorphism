e1cdde314303a1a0ba7d03c737ce3b8a9fb8cc94
riham <riham91.alsiyabi@gmail.com>
2025-07-23 14:51:13 +0400

    public bool IsLuxury { get; set; } // Indicates if the car is a luxury model

--- Car.cs
+++ Car.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SmartCarRentalsSystemPolymorphism
 {
-    class Car
+    public class Car : Vehicle
     {
+        public bool IsLuxury { get; set; } // Indicates if the car is a luxury model
+
+        public Car(string brand, string model, int year, string licensePlate, bool isLuxury)
+            : base(brand, model, year, licensePlate)
+        {
+            IsLuxury = isLuxury;
+        }
     }
 }
