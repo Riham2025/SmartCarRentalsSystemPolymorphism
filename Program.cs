7a61e81e8d61c2babf45e06f7eb1ea59ac41fa46
riham <riham91.alsiyabi@gmail.com>
2025-07-24 07:16:18 +0400

    Step 2: Show menu

--- Program.cs
+++ Program.cs
@@ -5,8 +5,26 @@
         static void Main(string[] args)
         {
             // Step 1: Create sample vehicles
-            List<Vehicle> fleet = new List<Vehicle>
+
+            List<Vehicle> fleet = new List<Vehicle> 
             {
+                new Car("Toyota", "Camry", 2022, "ABC123", false),
+                new Car("BMW", "7 Series", 2020, "BMW700", true),
+                new Truck("Volvo", "FH16", 2018, "TRK890", 15000),
+                new Motorbike("Yamaha", "MT-07", 2021, "BIKE07", true)
+            };
+
+            bool running = true;
+
+            while (running)
+            {
+                //Step 2: Show menu
+                Console.WriteLine("\n==== SmartCar Rental System ====");
+                Console.WriteLine("1. View All Vehicles");
+                Console.WriteLine("2. Rent a Vehicle");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
             }
     }
 }
